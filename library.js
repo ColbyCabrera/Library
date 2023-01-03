176f8ebcf5322c95123d2a416f55058cfb9f3e5b
ColbyCabrera <gdthyispro@gmail.com>
2023-01-03 14:40:13 -0500

    Take input from form and add book to book list

--- library.js
+++ library.js
@@ -1,5 +1,17 @@
 let myLibrary = [];
 const bookContainer = document.getElementById("book-container");
+const newBookBtn = document.getElementById("new-book-btn");
+newBookBtn.addEventListener("click", submitClick);
+
+function submitClick(event) {
+  event.preventDefault();
+  const title = document.getElementById("book-title").value;
+  const author = document.getElementById("book-author").value;
+  const pages = document.getElementById("book-pages").value;
+  const isRead = document.getElementById("is-read").checked;
+
+  addBookToLibrary(title, author, pages, isRead);
+}
 
 function Book() {
   this.info = function () {
@@ -8,16 +20,23 @@ function Book() {
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-  book = new Book();
+  let book = new Book();
   book.title = title;
   book.author = author;
   book.pages = pages;
   book.isRead = isRead;
 
   myLibrary.push(book);
+  displayBooks();
 }
 
 function displayBooks() {
+  const displayedBooks = Array.from(document.getElementsByClassName("book"));
+
+  displayedBooks.forEach((element) => {
+    element.parentElement.removeChild(element);
+  });
+
   myLibrary.forEach((book) => {
     const newBook = document.createElement("div");
     const bookTitle = document.createElement("h3");
@@ -31,20 +50,14 @@ function displayBooks() {
     if (book.isRead === true) {
       isRead.textContent = "Read";
     } else {
-        isRead.textContent = "Not read";
+      isRead.textContent = "Not read";
     }
 
-    
-
     bookContainer.appendChild(newBook);
+    newBook.className = "book";
     newBook.appendChild(bookTitle);
     newBook.appendChild(bookAuthor);
     newBook.appendChild(bookPages);
     newBook.appendChild(isRead);
   });
 }
-
-addBookToLibrary("Lord of The Rings", "Tolkien", "900", true);
-addBookToLibrary("Book1", "Tolkien", "900", true);
-
-displayBooks();
